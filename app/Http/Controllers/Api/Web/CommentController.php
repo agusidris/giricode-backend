823364cb35fe6bfa2c69ce8f8c72ea6231cdc698
magusidris <magusidris@gmail.com>
2021-10-25 17:16:29 +0700

    Add return fail data comment

--- app/Http/Controllers/Api/Web/CommentController.php
+++ app/Http/Controllers/Api/Web/CommentController.php
@@ -30,42 +30,34 @@ class CommentController extends Controller
     {
         $userId = auth()->user();
 
-        if ($userId) {
-            $comment = new Comment;
-            $comment->comment = $request->comment;
-            $comment->user()->associate($userId);
-            $post->comments()->save($comment);
+        $comment = new Comment;
+        $comment->comment = $request->comment;
+        $comment->user()->associate($userId);
+        $post->comments()->save($comment);
 
-            if($post) {
-                //return success with Api Resource
-                return new CommentResource(true, 'Comment Berhasil Disimpan!', null);
-            }
-
-            return new CommentResource(false, 'Comment Gagal Disimpan!', null);
+        if($post) {
+            //return success with Api Resource
+            return new CommentResource(true, 'Comment Berhasil Disimpan!', null);
         }
 
-        return new CommentResource(false, 'Login dulu', null);
+        return new CommentResource(false, 'Comment Gagal Disimpan!', null);
     }
 
     public function reply(Post $post, Request $request)
     {
         $userId = auth()->user();
 
-        if ($userId) {
-            $reply = new Comment;
-            $reply->comment = $request->comment;
-            $reply->user()->associate($userId);
-            $reply->parent_id=$post->id;
-            $post->comments()->save($reply);
+        $reply = new Comment;
+        $reply->comment = $request->comment;
+        $reply->user()->associate($userId);
+        $reply->parent_id=$post->id;
+        $post->comments()->save($reply);
 
-            if($post) {
-                //return success with Api Resource
-                return new CommentResource(true, 'Reply berhasil Disimpan!', null);
-            }
-
-            return new CommentResource(true, 'Reply Gagal Disimpan!', null);
+        if($post) {
+            //return success with Api Resource
+            return new CommentResource(true, 'Reply berhasil Disimpan!', null);
         }
 
-        return new CommentResource(false, 'Login dulu', null);
+        return new CommentResource(true, 'Reply Gagal Disimpan!', null);
     }
 }
